54601b5f3be952e27fae6e4f7e28527a8f305c5d
Juan-sebas370 <sebastian.pinilla.arcila@gmail.com>
2025-03-28 10:20:58 -0500

    html y css

---
+++ scripts/colibri.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const observador = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            }
+        });
+    });
+    document.querySelectorAll('section').forEach((section) => {
+        observador.observe(section);
+    });
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
